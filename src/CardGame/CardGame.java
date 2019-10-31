89789873253c2a8d53c4f41db1678ae3b86a49ef
Fabio Alpizar <josefabiorocks@gmail.com>
2019-10-31 06:16:31 -0600

    json y creacion de weapons and characters listo

---
+++ src/CardGame/CardGame.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+import CharacterApi.ICharacter;
+import CharacterApi.IWeapon;
+import CharacterApi.Weapon;
+import Loader.*;
+import java.util.List;
+
+/**
+ *
+ * @author Pumkin
+ */
+public class CardGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        WeapJsonLoader weaponLoader = new WeapJsonLoader();
+        List<IWeapon> weaponList = weaponLoader.load("src\\Resources\\weapons.json");
+        
+        CharacterJsonLoader charLoader = new CharacterJsonLoader(weaponList);
+        List<ICharacter> characterList = charLoader.load("src\\Resources\\characters.json");
+        
+        for(ICharacter C : characterList){
+            System.out.println(C.getName());   
+        }
+        
+    }
+    
+}
