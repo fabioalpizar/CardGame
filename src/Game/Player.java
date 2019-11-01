28533f783fb894d1ca2dc5f15a8c0fead4f33b21
raquel180196 <39704480+raquel180196@users.noreply.github.com>
2019-11-01 02:20:21 -0600

    game manager update

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,6 +1,8 @@
 package Game;
 
+import CharacterApi.ICharacter;
 import ObserverPattern.IObserver;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,11 +19,25 @@ import ObserverPattern.IObserver;
 public class Player implements IObserver{
     private int ID;
     private String username;
+    private int winnerGame;
+    private int loseGame;
+    private int giveUps;
+    private int loseAttacks;
+    private int winnerAttacks;
+    private ArrayList<ICharacter> characters;
 
-    public Player(int ID, String username) {
+    public Player(int ID, String username, int winnerGame, int loseGame, int giveUps, int loseAttacks, int winnerAttacks, ArrayList<ICharacter> characters) {
         this.ID = ID;
         this.username = username;
+        this.winnerGame = winnerGame;
+        this.loseGame = loseGame;
+        this.giveUps = giveUps;
+        this.loseAttacks = loseAttacks;
+        this.winnerAttacks = winnerAttacks;
+        this.characters = characters;
     }
+
+    
 
     public int getID() {
         return ID;
@@ -38,6 +54,55 @@ public class Player implements IObserver{
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public int getWinnerGame() {
+        return winnerGame;
+    }
+
+    public void setWinnerGame(int winnerGame) {
+        this.winnerGame = winnerGame;
+    }
+
+    public int getLoseGame() {
+        return loseGame;
+    }
+
+    public void setLoseGame(int loseGame) {
+        this.loseGame = loseGame;
+    }
+
+    public int getGiveUps() {
+        return giveUps;
+    }
+
+    public void setGiveUps(int giveUps) {
+        this.giveUps = giveUps;
+    }
+
+    public int getLoseAttacks() {
+        return loseAttacks;
+    }
+
+    public void setLoseAttacks(int loseAttacks) {
+        this.loseAttacks = loseAttacks;
+    }
+
+    public int getWinnerAttacks() {
+        return winnerAttacks;
+    }
+
+    public void setWinnerAttacks(int winnerAttacks) {
+        this.winnerAttacks = winnerAttacks;
+    }
+
+    public ArrayList<ICharacter> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(ArrayList<ICharacter> characters) {
+        this.characters = characters;
+    }
+    
 
     @Override
     public void notifyObserver(String command, Object source) {
